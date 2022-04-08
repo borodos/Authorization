00603381c026577d57955e794e5e7ae2097e4158
borodos <stepllerrus78@gmail.com>
2022-04-08 18:42:44 +0300

    ьлдж

--- script.js
+++ script.js
@@ -6,24 +6,35 @@ const button = document.querySelector("#btn");
 // form.action = "https://google.com/search";
 // form.submit();
 // console.log(button);
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
+button.addEventListener("onclick", (e) => {
+	e.target.preventDefault();
 	let formData = new FormData(form);
-	console.log(formData.body);
+	console.log(formData);
 });
-// method: "POST",
-// headers: {
-// 	"Content-Type": "application/x-www-form-urlencoded",
-// },
 
-for (let index = 0; index < form.length; index++) {
-	const element = form[index];
-	console.log(element);
+const btnRequest = document.querySelector("#btnRequest");
+
+btnRequest.addEventListener("click", func);
+
+async function func(e) {
+	e.preventDefault();
+
+	let response = await fetch("http://localhost:8000/", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+	});
+
+	/** -- Для получения тела ответа нам нужно использовать дополнительный вызов метода
+	 * response.text() – читает ответ и возвращает как обычный текст,
+	 * response.json() – декодирует ответ в формате JSON,
+	 * response.formData() – возвращает ответ как объект FormData (разберём его в следующей главе),
+	 * response.blob() – возвращает объект как Blob (бинарные данные с типом),
+	 * response.arrayBuffer() – возвращает ответ как ArrayBuffer (низкоуровневое представление бинарных данных),
+	 * помимо этого, response.body – это объект ReadableStream, с помощью которого можно считывать тело запроса по частям.
+	 */
+	let result = await response.json();
+	console.log(result);
 }
-
-// async function sendReq() {
-// 	let response = await fetch("http://localhost:3000/", {
-// 		method: "GET",
-// 	});
-// 	let result = await response.json();
-// }
